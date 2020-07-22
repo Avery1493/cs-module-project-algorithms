6923aae75d091b950a6d95b798bbd7e0961267e7
Avery Quinn <avery.quinn1493@gmail.com>
2020-07-22 15:02:54 -0700

    moving 0s

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,19 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    # Plan
+    # Count zeros in arr
+    count = arr.count(0)
+    # remove zeros from arr
+    while 0 in arr:
+        arr.remove(0)
+    # if zero >= 1:
+    while count > 0:
+        # append zero * count to end of arr
+        arr.append(0)
+        count -= 1
+    # return arr
+    return arr
 
 
 if __name__ == '__main__':
